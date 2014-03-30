88700c6efe2940dff68e6920ac67b34596fbca95
"Jason Scheirer" <jason.scheirer@gmail.com>
2014-03-30 12:36:01 -0700

    Cleaning up before I leave for the day

--- oauthworkflow/agoworkflow.go
+++ oauthworkflow/agoworkflow.go
@@ -2,11 +2,14 @@ package oauthworkflow
 
 import "fmt"
 import "net/http"
+import "net/url"
 
 type AGOLogin struct {
-	port    uint32
-	success chan string
-	error   chan string
+	APPID     string
+	APPSECRET string
+	port      uint32
+	success   chan string
+	error     chan string
 }
 
 func (self AGOLogin) InitializeOAuthFlow(port uint32, success chan string,
@@ -17,16 +20,32 @@ func (self AGOLogin) InitializeOAuthFlow(port uint32, success chan string,
 }
 
 func (self AGOLogin) FirstURL() string {
-	return fmt.Sprintf("http://127.0.0.1:%v", self.port)
+	redirect_uri := url.QueryEscape(fmt.Sprintf("http://127.0.0.1:%v/gotLogin", self.port))
+	url := fmt.Sprintf("https://www.arcgis.com/sharing/oauth2/authorize?client_id=%v&response_type=code&redirect_uri=%v", self.APPID, redirect_uri)
+
+	return url
 }
 
 func (self AGOLogin) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
-	if req.URL.Path == "/" {
-
-	} else if req.URL.Path == "/gotLogin" {
+	if req.URL.Path == "/gotLogin" {
 		code := req.URL.Query().Get("code")
 		if code != "" {
-			response := "You are now logged in."
+			// TODO
+			/* Access token
+
+			Once the authorization code has been obtained, the app needs to exchange it for an access token.
+
+			The actual request is a POST request to the /token endpoint for the portal, shown here for arcgis.com:
+
+			https://www.arcgis.com/sharing/oauth2/token
+			All the parameters (in the following example) must be sent in the request body and not as part of the query component of the URI.
+
+			client_id=APPID&
+			client_secret=APPSECRET&
+			grant_type=authorization_code&
+			code=CODE_OBTAINED_IN_THE_PREVIOUS_STEP */
+
+			response := "You are now logged in. You can close this window."
 			writer.Write([]byte(response))
 
 			self.success <- code
@@ -41,9 +60,9 @@ func (self AGOLogin) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 			self.error <- error
 		}
 
-		http.Redirect(writer, req, "/", 303)
+		http.Redirect(writer, req, self.FirstURL(), 303)
 
 	} else {
-		http.Redirect(writer, req, "/", 303)
+		http.Redirect(writer, req, self.FirstURL(), 303)
 	}
 }
