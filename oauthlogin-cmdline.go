fc8d794223ef16265c1aa489453da12453d3faeb
"Jason Scheirer" <jason.scheirer@gmail.com>
2014-05-26 13:57:51 -0700

    Add new commandline app with args

---
+++ oauthlogin-cmdline.go
@@ -0,0 +1,34 @@
+// Command-line AGOL login app that has everything set as options
+package main
+
+import "flag"
+import "time"
+import "fmt"
+import "os"
+
+import "./oauthworkflow"
+
+func main() {
+	var port, timeout_in_seconds uint64
+	var APPID, APPSECRET string
+
+	flag.Uint64Var(&port, "port", 8327, "Port to bind to locally")
+	flag.Uint64Var(&timeout_in_seconds, "timeout", 30, "Timeout (in seconds)")
+	flag.StringVar(&APPID, "appid", "<APPID>", "App ID")
+	flag.StringVar(&APPSECRET, "appsecret", "<APPSECRET>", "App Secret")
+	flag.Parse()
+
+	timeout := time.Duration(timeout_in_seconds) * time.Second
+	agoflow := oauthworkflow.MakeAGOFlow(APPID, APPSECRET)
+
+	success, error := oauthworkflow.FullOAuthHandshake(agoflow, timeout,
+		uint32(port))
+
+	if success != "" {
+		fmt.Print(success)
+	} else if error != "" {
+		fmt.Fprintln(os.Stderr, error)
+	} else {
+		fmt.Println(os.Stderr, "Unspecified error")
+	}
+}
